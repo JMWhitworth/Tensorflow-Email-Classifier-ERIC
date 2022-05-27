7ad9e1178a693c9c8b724868ca07c1c47e845c4c
Jack Whitworth <45968087+Jack-Writes-Code@users.noreply.github.com>
2022-05-27 12:51:58 +0000

    testing codespaces

--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from eric_core import Eric
 
 if __name__ == "__main__":
     eric = Eric(training=True)
-    print(eric.predict(["hello@jackwhitworth.co.uk"]))
+    print(eric.predict(["jack@inventis.uk"]))
