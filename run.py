6bd33b57835dc6a2cfbdb245cea11ccf0ccfbdba
Jack Whitworth <jack.whitworth@substrakt.com>
2023-10-13 16:34:21 +0100

    UPDATE: Clearer example which doesn't retrain unless needed

--- run.py
+++ run.py
@@ -1,5 +1,25 @@
 from eric_core import Eric
+import os
 
 if __name__ == "__main__":
-    eric = Eric(training=True)
-    print(eric.predict(["jack@inventis.uk"]))
+    # Is there a saved, trained model?
+    existingModel = os.path.exists('./model')
+
+    # Instantiate ERIC and train if no model exists
+    eric = Eric(training=not existingModel)
+
+    # List of values to test
+    testEmails = [
+        "jack@jackwhitworth.com",
+        "hello, world!"
+    ]
+
+    # Test the list and save results
+    predictions = eric.predict(testEmails)
+    
+    # Output the results in a human-friendly way
+    for i in range(0, len(predictions)):
+        if predictions[i]:
+            print(f"{testEmails[i]} is an email.")
+        else:
+            print(f"{testEmails[i]} is not an email.")
